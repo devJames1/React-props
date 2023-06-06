cfc29b70e2b376036ae44b66b47aa71b19ed8a97
James Okolie <zebedee788@gmail.com>
2023-06-06 18:10:25 +0600

    On branch master  Changes to be committed: 	modified:   src/App.js 	new file:   src/Classprops.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Classprops from './Classprops';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Classprops name="learner 1" place="placeX">
+        <p>Child Component</p>
+      </Classprops>
+      <Classprops name="learner 2" place="placeY">
+        <button>Click</button>
+      </Classprops>
+      <Classprops name="learner 3" place="placeZ" />
     </div>
   );
 }
